971c5665489c87e392148ed0d3c4c39c5b360a8a
Bernhard Brodowsky <brodowsb@ethz.ch>
2015-10-18 20:18:56 +0200

    Added CompilerTest.

--- Test.hs
+++ Test.hs
@@ -2,8 +2,10 @@ import ParserTest
 import SyntaxTest
 import TypeCheckerTest
 import CompilerTest
+import RegressionTest
 
 main = do
+  regressionCheck
   compilerCheck
   parserCheck
   syntaxCheck
