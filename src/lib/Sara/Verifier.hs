a5ae3af1d7a235f32c4383e38c1a6998e606b544
Bernhard Brodowsky <brodowsb@ethz.ch>
2015-11-01 16:44:59 +0100

    Finished first step of Z3 expressions.

--- src/lib/Sara/Verifier.hs
+++ src/lib/Sara/Verifier.hs
@@ -1,75 +1,48 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module Sara.Verifier (z3Expression, Z3FullContext(..), Z3CheapContext(..), FunctionInfo(..), PrecAST(..), z3PrecExpression) where
+module Sara.Verifier ( z3Expression ) where
 
+import Sara.Z3AstUtils
+import Data.List
 import Sara.Operators
 import qualified Sara.Syntax as S
+import qualified Control.Monad.Trans.Reader as R
 import Sara.Types
 import Sara.Meta
-import Data.Maybe
-import qualified Control.Monad.Trans.Reader as R
 import Control.Monad.Trans.Class
-import Control.Monad.Reader.Class
-import Control.Monad
 import Z3.Monad
-
-import qualified Data.Map.Strict as M
-
-data FunctionKey =
-  FunctionKey S.Name [Type]
-  deriving (Eq, Ord, Show)
-
--- | Context for the second pass which has all the pre- and postconditions.
-data Z3FullContext
-  = Z3FullContext { fullVars :: M.Map S.Name AST
-                  , funcs :: M.Map FunctionKey FunctionInfo }
-
-data FunctionInfo =
-  FunctionInfo { precondition :: AST
-               , postcondition :: AST }
-
--- | Context for the first pass which has only the signatures.
--- I.e. we don't have the preconditions yet.
-data Z3CheapContext
-  = Z3CheapContext { cheapVars :: M.Map S.Name AST
-                   , signatures :: M.Map FunctionKey TypeCheckerSignature }
 
 instance MonadZ3 m => MonadZ3 (R.ReaderT a m) where
   getSolver = lift getSolver
   getContext = lift getContext
 
--- | An AST and his precondition.
-data PrecAST = PrecAST { prec :: AST, ast :: AST }
+z3Sort :: MonadZ3 z3 => Type -> z3 Sort
+z3Sort Boolean = mkBoolSort
+z3Sort Integer = mkIntSort
+z3Sort _       = undefined
 
--- | A trivial expression with no precondition.
-z3PrecTrivial :: MonadZ3 z3 => z3 AST -> z3 PrecAST
-z3PrecTrivial ast = PrecAST <$> mkTrue <*> ast
+z3Symbol :: MonadZ3 z3 => String -> String -> Int -> z3 Symbol
+z3Symbol prefix name index = mkStringSymbol $ intercalate "$" [prefix, name, show index]
 
--- | Create an expression and its precondition.
-z3PrecExpression :: (MonadZ3 z3, MonadReader Z3FullContext z3) => TypeCheckerExpression -> z3 PrecAST
-z3PrecExpression (S.Boolean b _)              = z3PrecTrivial $ mkBool b
-z3PrecExpression (S.Integer n _)              = z3PrecTrivial $ mkInteger n
-z3PrecExpression (S.UnaryOperation op e _)    = do
-  PrecAST p e' <- z3PrecExpression e
-  PrecAST p <$> z3UnOp op e'
-z3PrecExpression (S.BinaryOperation op l r _) = do
-  l' <- z3PrecExpression l
-  r' <- z3PrecExpression r
-  z3PrecBinOp op l' r'
-z3PrecExpression (S.Variable v _ _)             = z3PrecTrivial $ asks $ fromJust . M.lookup v . fullVars
-z3PrecExpression (S.Conditional b t e _)      = do
-  PrecAST p b' <- z3PrecExpression b
-  PrecAST q t' <- z3PrecExpression t
-  PrecAST r e' <- z3PrecExpression e
-  q' <- mkImplies b' q
-  r' <- join $ mkImplies <$> mkNot b' <*> pure r
-  PrecAST <$> mkAnd [p, q', r'] <*> mkIte b' t' e'
-z3PrecExpression _                              = undefined
+z3Var :: MonadZ3 z3 => VariableMeta -> Type -> z3 AST
+z3Var (VariableMeta name index) typ = do
+  sym <- z3Symbol "var" name index
+  sort <- z3Sort typ
+  mkVar sym sort
 
--- | Create an expression without its precondition.
-z3Expression :: (MonadZ3 z3, MonadReader Z3CheapContext z3) => TypeCheckerExpression -> z3 AST
-z3Expression (S.Boolean b _)              = mkBool b
-z3Expression (S.Integer n _)              = mkInteger n
+z3FuncDecls :: MonadZ3 z3 => FunctionMeta -> [Type] -> Type -> z3 (FuncDecl, FuncDecl, FuncDecl)
+z3FuncDecls (FunctionMeta name index) argTypes retType = do
+  preSym <- z3Symbol "pre" name index
+  postSym <- z3Symbol "post" name index
+  funcSym <- z3Symbol "func" name index
+  argSorts <- mapM z3Sort argTypes
+  retSort <- z3Sort retType
+  (,,) <$> mkFuncDecl preSym argSorts retSort <*> mkFuncDecl postSym argSorts retSort <*> mkFuncDecl funcSym argSorts retSort
+
+-- | Create an expression and its precondition and postcondition.
+z3Expression :: MonadZ3 z3 => SymbolizerExpression -> z3 CondAST
+z3Expression (S.Boolean b _)              = trivial =<< mkBool b
+z3Expression (S.Integer n _)              = trivial =<< mkInteger n
 z3Expression (S.UnaryOperation op e _)    = do
   e' <- z3Expression e
   z3UnOp op e'
@@ -77,13 +50,22 @@ z3Expression (S.BinaryOperation op l r _) = do
   l' <- z3Expression l
   r' <- z3Expression r
   z3BinOp op l' r'
-z3Expression (S.Variable v _ _)             = asks $ fromJust . M.lookup v . cheapVars
-z3Expression (S.Conditional b t e _)      = do
-  b' <- z3Expression b
-  t' <- z3Expression t
-  e' <- z3Expression e
-  mkIte b' t' e'
-z3Expression _                              = undefined
+z3Expression v@(S.Variable _ m _)         = trivial =<< z3Var m (expressionTyp v)
+z3Expression (S.Conditional c t e _)      = do
+  c' <- z3Expression c
+  t' <- conditionOn (ast c') =<< z3Expression t
+  e' <- conditionOnNot (ast c') =<< z3Expression e
+  combine3 mkIte c' t' e'
+z3Expression c@(S.Call _ a m _)           = do
+  a' <- mapM z3Expression a
+  let args = map ast a'
+  let aTyps = map expressionTyp a
+  let rTyp = expressionTyp c
+  (funcPre, funcPost, func) <- z3FuncDecls m aTyps rTyp
+  pre <- mkApp funcPre args
+  post <- mkApp funcPost args
+  addPrecondition pre =<< addPostcondition post =<< combine (mkApp func) a'
+z3Expression _                            = undefined
 
 mkZero :: MonadZ3 z3 => z3 AST
 mkZero = mkInteger 0
@@ -91,58 +73,45 @@ mkZero = mkInteger 0
 mkNeZero :: MonadZ3 z3 => AST -> z3 AST
 mkNeZero b = mkNot =<< mkEq b =<< mkZero
   
-z3UnOp :: MonadZ3 z3 => UnaryOperator -> AST -> z3 AST
-z3UnOp UnaryPlus e  = return e
-z3UnOp UnaryMinus e = do
-  z <- mkZero
-  mkSub [z, e]
-z3UnOp LogicalNot e = mkNot e
-z3UnOp _ _          = undefined
+z3UnOp :: MonadZ3 z3 => UnaryOperator -> CondAST -> z3 CondAST
+z3UnOp op = liftCond $ z3UnOp' op
+  where z3UnOp' UnaryPlus e  = return e
+        z3UnOp' UnaryMinus e = do
+          z <- mkZero
+          mkSub [z, e]
+        z3UnOp' LogicalNot e = mkNot e
+        z3UnOp' _ _          = undefined
 
 -- | Apply a function that takes a list to two arguments.
 app2 :: ([a] -> b) -> a -> a -> b
 app2 f a b = f [a, b]
 
--- | For a two argument function that transforms two AST to an AST, just conjunct the preconditions.
-forwardPrecs2 :: MonadZ3 z3 => (AST -> AST -> z3 AST) -> PrecAST -> PrecAST -> z3 PrecAST
-forwardPrecs2 f (PrecAST p a) (PrecAST q b) = PrecAST <$> mkAnd [p, q] <*> f a b
+z3DivOp :: MonadZ3 z3 => (AST -> AST -> z3 AST) -> CondAST -> CondAST -> z3 CondAST
+z3DivOp f l r = do
+  neZero <- mkNeZero (ast r)
+  result <- combine2 f l r
+  addPrecondition neZero result
 
-z3PrecDivOp :: MonadZ3 z3 => (AST -> AST -> z3 AST) -> PrecAST -> PrecAST -> z3 PrecAST
-z3PrecDivOp f (PrecAST p a) (PrecAST q b) = do
-  neZero <- mkNeZero b
-  PrecAST <$> mkAnd [p, neZero, q] <*> f a b
+appConds2 :: MonadZ3 z3 => ([AST] -> z3 AST) -> CondAST -> CondAST -> z3 CondAST
+appConds2 = combine2 . app2
 
-appPrecs2 :: MonadZ3 z3 => ([AST] -> z3 AST) -> PrecAST -> PrecAST -> z3 PrecAST
-appPrecs2 = forwardPrecs2 . app2
-
-z3BinOp :: MonadZ3 z3 => BinaryOperator -> AST -> AST -> z3 AST
-z3BinOp op a b = ast <$> z3TrivialPrecBinOp
-  where z3TrivialPrecBinOp :: MonadZ3 z3 => z3 PrecAST
-        z3TrivialPrecBinOp = join $ z3PrecBinOp op <$> z3PrecTrivial (return a) <*> z3PrecTrivial (return b)
-
-z3PrecBinOp :: MonadZ3 z3 => BinaryOperator -> PrecAST -> PrecAST -> z3 PrecAST
-z3PrecBinOp Times           = appPrecs2 mkMul
-z3PrecBinOp DividedBy       = z3PrecDivOp mkDiv
-z3PrecBinOp Modulo          = z3PrecDivOp mkMod
-z3PrecBinOp Plus            = appPrecs2 mkAdd
-z3PrecBinOp Minus           = appPrecs2 mkSub
-z3PrecBinOp LessThan        = forwardPrecs2 mkLt
-z3PrecBinOp AtMost          = forwardPrecs2 mkLe
-z3PrecBinOp GreaterThan     = forwardPrecs2 mkGt
-z3PrecBinOp AtLeast         = forwardPrecs2 mkGe
-z3PrecBinOp EqualTo         = forwardPrecs2 mkEq
-z3PrecBinOp NotEqualTo      = forwardPrecs2 $ \a b ->
-  mkNot =<< mkEq a b
-z3PrecBinOp LogicalAnd      = \(PrecAST p a) (PrecAST q b) -> do
-  q' <- mkImplies a q
-  PrecAST <$> mkAnd [p, q'] <*> mkAnd [a, b]
-z3PrecBinOp LogicalXor      = forwardPrecs2 $ mkXor
-z3PrecBinOp LogicalOr       =  \(PrecAST p a) (PrecAST q b) -> do
-  q' <- join $ mkImplies <$> mkNot a <*> pure q
-  PrecAST <$> mkAnd [p, q'] <*> mkOr [a, b]
-z3PrecBinOp Implies         = forwardPrecs2 $ mkImplies
-z3PrecBinOp ImpliedBy       = forwardPrecs2 $ flip mkImplies
-z3PrecBinOp EquivalentTo    = forwardPrecs2 $ mkEq
-z3PrecBinOp NotEquivalentTo = forwardPrecs2 $ \a b ->
-  mkNot =<< mkEq a b
-z3PrecBinOp _               = undefined
+z3BinOp :: MonadZ3 z3 => BinaryOperator -> CondAST -> CondAST -> z3 CondAST
+z3BinOp Times           = appConds2 mkMul
+z3BinOp DividedBy       = z3DivOp mkDiv
+z3BinOp Modulo          = z3DivOp mkMod
+z3BinOp Plus            = appConds2 mkAdd
+z3BinOp Minus           = appConds2 mkSub
+z3BinOp LessThan        = combine2 mkLt
+z3BinOp AtMost          = combine2 mkLe
+z3BinOp GreaterThan     = combine2 mkGt
+z3BinOp AtLeast         = combine2 mkGe
+z3BinOp EqualTo         = combine2 mkEq
+z3BinOp NotEqualTo      = combine2 $ \a b -> mkNot =<< mkEq a b
+z3BinOp LogicalAnd      = \a b -> combine2 (app2 mkAnd) a =<< conditionOn (ast a) b
+z3BinOp LogicalXor      = combine2 $ mkXor
+z3BinOp LogicalOr       = \a b -> combine2 (app2 mkOr) a =<< conditionOnNot (ast a) b
+z3BinOp Implies         = \a b -> combine2 mkImplies a =<< conditionOn (ast a) b
+z3BinOp ImpliedBy       = \a b -> combine2 (flip mkImplies) b =<< conditionOn (ast b) a
+z3BinOp EquivalentTo    = combine2 $ mkEq
+z3BinOp NotEquivalentTo = combine2 $ \a b -> mkNot =<< mkEq a b
+z3BinOp _               = undefined
