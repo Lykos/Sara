2dd2d4aa596b340d63260a5d4977b11ac54312d1
Bernhard Brodowsky <brodowsb@ethz.ch>
2015-11-03 20:22:56 +0100

    Use Franks version for the tricky function.

--- src/lib/Sara/Utils.hs
+++ src/lib/Sara/Utils.hs
@@ -1,11 +1,7 @@
 -- | Module for all the common Haskell functions that have nothing to do with a compiler.
 module Sara.Utils (foldlM) where
 
-import Control.Monad
-
 -- TODO Improve! Nobody understands this, not even yourself! And it is ugly! Probably it is possible to do a nice version.
+-- Try to at least understand what the variables mean and give them appropriate names.
 foldlM :: Monad m => (a -> m (m b -> m b)) -> [a] -> m b -> m b
-foldlM f xs m = join $ foldM f' m xs
-  where f' m x = do
-          f'' <- f x
-          return $ f'' m
+foldlM f xs m = foldl (\ a b -> (f b) >>= ($a)) m xs
