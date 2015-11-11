eb4cb930b77117861a31576b925032ba907550f7
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2015-11-11 18:25:46 +0100

    Empty is a pattern and not a special value for AstWrapper.

--- src/lib/Sara/Z3/AstWrapper.hs
+++ src/lib/Sara/Z3/AstWrapper.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE PatternSynonyms #-}
 
 -- | Data structures to keep track of some properties of the Z3 AST structure.
 module Sara.Z3.AstWrapper ( ProofObligation
@@ -16,17 +17,18 @@ module Sara.Z3.AstWrapper ( ProofObligation
 
 import Sara.Z3.Utils
 import Data.Maybe
-import Z3.Monad
 import Sara.Errors (VerifierFailureType)
 import Text.Parsec.Pos (SourcePos)
+import Z3.Monad
 
 -- | AST structure container for which a failure can be tracked to the failing part.
 data TrackableAST a
-  = Empty
-  | Singleton AST a
+  = Singleton AST a
   | Conjunction [TrackableAST a]
   | Condition AST (TrackableAST a)
   deriving (Eq, Ord, Show)
+
+pattern Empty = Conjunction []
 
 -- | AST structure container for which a failure can be tracked.
 type FailureTrackableAST = TrackableAST (VerifierFailureType, SourcePos)
@@ -35,13 +37,13 @@ class ASTWrapper a b | a -> b where
   unwrap :: a -> TrackableAST b
   wrap :: TrackableAST b -> a
 
--- | Ast Wrapper for proof obligations.
+-- | AST Wrapper for proof obligations.
 -- We use this to store additional information with the AST and to ensure better type safety.
 newtype ProofObligation
   = ProofObligation { unObl :: FailureTrackableAST }
   deriving (Eq, Ord, Show)
 
--- | Ast Wrapper for assumptions.
+-- | AST Wrapper for assumptions.
 -- We use this to for better type safety.
 newtype Assumption
   = Assumption { unAss :: TrackableAST () }
@@ -67,8 +69,7 @@ singleton ast = wrap . Singleton ast
 
 conjunct :: ASTWrapper a b => [a] -> a
 conjunct as = wrap $ Conjunction $ concatMap extract $ map unwrap $ as
-  where extract Empty            = []
-        extract (Conjunction as) = as
+  where extract (Conjunction as) = as
         extract a                = [a]
 
 conditionOn :: ASTWrapper a b => AST -> a -> a
@@ -79,15 +80,15 @@ conditionOn ast = wrap . conditionOn' . unwrap
 runAst :: (ASTWrapper a b, MonadZ3 z3) => a -> z3 AST
 runAst a = runAst' (unwrap a)
   where runAst' Empty             = mkTrue
-        runAst' (Singleton ast _) = return ast
+        runAst' (Conjunction [a]) = runAst' a
         runAst' (Conjunction as)  = conjunctAsts =<< mapM runAst' as
+        runAst' (Singleton ast _) = return ast
         runAst' (Condition ast a) = mkImplies ast =<< runAst' a
 
 runProofObligation :: ProofObligation -> FailureTrackableAST
 runProofObligation = unwrap
 
 findFailure :: MonadZ3 z3 => Model -> TrackableAST a -> z3 (Maybe a)
-findFailure _ Empty                      = return Nothing
 findFailure model (Singleton ast result) = do
   val <- evalBool model ast
   case val of
